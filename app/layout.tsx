ec8037fd9175bd96b8cf283b1ec4d0d420555f76
Nyareth Marko <yuyuvybz@gmail.com>
2024-01-01 23:32:36 +0200

    Worked on SEO

--- app/layout.tsx
+++ app/layout.tsx
@@ -9,8 +9,33 @@ import { Analytics } from "@vercel/analytics/react";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "Nyareth Marko",
-  description: "This is my portfolio",
+  title: "Nyareth Marko's Portfolio",
+  description:
+    "Nyareth Marko is a 17 year old software engineer with more than 5years of experience in full stack software development",
+  creator: "Nyareth Marko",
+  keywords: [
+    "Nyareth",
+    "Marko",
+    "Nyareth Marko",
+    "Software Engineer",
+    "Software Developer",
+    "Full Stack Developer",
+    "Full Stack Engineer",
+    "Full Stack",
+    "Developer",
+    "Engineer",
+    "Software",
+    "Nyareth Marko's Portfolio",
+    "Nyareth Marko's Website",
+    "Nyareth Marko's Blog",
+    "Software development in South Sudan",
+    "Sudanese programmers",
+    "South Sudanese programmers",
+    "Sudanese developers",
+    "South Sudanese developers",
+    "Sudanese software engineers",
+    "Best software developers in South Sudan",
+  ],
 };
 
 export default function RootLayout({
